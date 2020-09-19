8eb9369b221ebc46c8ac1d8ad0a3a864591159f9
windhooked <hannesdewaal@gmail.com>
2020-09-19 17:08:55 +0200

    test Xon Xoff

--- serial/open_windows_test.go
+++ serial/open_windows_test.go
@@ -7,12 +7,18 @@ import (
 	"os/signal"
 	"syscall"
 	"testing"
+	"time"
+)
+
+const (
+	XOFF = 0x11
+	XON  = 0x13
 )
 
 // requires com0com virtual port or similar
 func TestXonXoff(t *testing.T) {
 	// open
-	portA, err := serial.Open(OpenOptions{
+	portA, err := Open(OpenOptions{
 		PortName:              "CNCA0",
 		BaudRate:              115200,
 		DataBits:              8,
@@ -24,16 +30,16 @@ func TestXonXoff(t *testing.T) {
 
 		XFlowControl: &XFlowControl{
 			TXContinueOnXOFF: true,
-			InX:              true,
+			InX:              false,
 			OutX:             true,
 		},
 	})
 	if err != nil {
-		t.Fatalf("Error opening serial port: ", err)
+		t.Fatalf("Error opening serial port: %v", err)
 	}
 	defer portA.Close()
 
-	portB, err := serial.Open(OpenOptions{
+	portB, err := Open(OpenOptions{
 		PortName:              "CNCB0",
 		BaudRate:              115200,
 		DataBits:              8,
@@ -52,49 +58,68 @@ func TestXonXoff(t *testing.T) {
 		},
 	})
 	if err != nil {
-		t.Fatalf("Error opening serial port: ", err)
+		t.Fatalf("Error opening serial port: %v", err)
 	}
 
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-	go func(portA serial.Port) {
-		buff := make([]byte, 512)
+	var transmitted []byte
+	var received []byte
+	go func(portA io.ReadWriteCloser) {
+		txBuff := make([]byte, 512)
 
-		for _, v := range sendBuf {
-			v = 0x55
+		for k, _ := range txBuff {
+			txBuff[k] = 0x55
 		}
+		txBuff[10] = XON
+		txBuff[20] = XOFF
 		loopCntr := 0
+	DONE:
 		for {
 			select {
 			case <-c:
 				os.Exit(1)
 			default:
-				w, err := portA.Write(out)
+				if loopCntr > 5 {
+					break DONE
+				}
+				in := []byte{0x00, 0x00}
+				n, err := portB.Read(in)
+				if n > 0 {
+					fmt.Printf("tx in %x\n", in)
+				}
+				w, err := portA.Write(txBuff)
 				if err != nil {
 					fmt.Printf("%v\n", err)
 				}
 				if w > 0 {
-					s, err := portA.GetModemStatusBits()
-					fmt.Printf("%v %v[%+v]: rx t (%v) (%v) % x\n", "A", loopCntr, s, len(out), w, out)
+					transmitted = append(transmitted, txBuff...)
+					//s, err := portA.GetModemStatusBits()
+					fmt.Printf("%v: tx total:%v %v % x\n", "A", loopCntr*len(txBuff), w, txBuff)
 					loopCntr++
 				}
 			}
 		}
 	}(portA)
 
-	go func(portB serial.Port) {
-		buff := make([]byte, 4096)
+	go func(portB io.ReadWriteCloser) {
+		buff := make([]byte, 200)
+		loopCntr := 0
 		for {
 			select {
 			case <-c:
 				os.Exit(1)
 			default:
+				// simulate busy slow reader
+				time.Sleep(time.Millisecond * 100)
 				n, err := portB.Read(buff)
 				if n > 0 {
 					out := buff[:n]
-					s, err := portB.GetModemStatusBits()
-					fmt.Printf("%v[%+v]: rx m (%v) (%v) % x\n", "B", s, len(out), w, out)
+					received = append(received, out[:n]...)
+					//s, err := portB.GetModemStatusBits()
+					fmt.Printf("%v: rx total:%v %v % x\n", "B", loopCntr*len(out), n, out)
+					loopCntr++
 				} else if err != nil && err != io.EOF {
 					fmt.Printf("%v\n", err)
 				}
@@ -103,6 +128,8 @@ func TestXonXoff(t *testing.T) {
 		}
 	}(portB)
 
+	fmt.Printf("% x", transmitted)
+	fmt.Printf("% x", received)
 	<-c
 
 }
